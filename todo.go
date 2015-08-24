21a0c419117bdd35d52a301419538bb5e9c0bc5f
Tien Thanh <thanhvt1609@gmail.com>
2015-08-24 23:10:40 +0700

    simple index page show all todo

--- todo.go
+++ todo.go
@@ -1,24 +1,84 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
+const (
+	dbUser string = "postgres"
+	dbPass string = "mashiro"
+	dbName        = "gotodo"
+)
+
+var db *sql.DB
+
+type Todo struct {
+	Id          int
+	Title       string
+	Description string
+	Created_at  time.Time
+}
+
 func main() {
+	var err error
+	dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPass, dbName)
+	db, err = sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.HandleFunc("/", serveTemplate)
+	http.HandleFunc("/", todosIndex)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
+func todosIndex(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM todo")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	defer rows.Close()
+
+	todos := make([]*Todo, 0)
+	for rows.Next() {
+		todo := new(Todo)
+		err := rows.Scan(&todo.Id, &todo.Title, &todo.Description, &todo.Created_at)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		todos = append(todos, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	indexTemplate(w, r, todos)
+}
+
+func indexTemplate(w http.ResponseWriter, r *http.Request, todos []*Todo) {
 	lp := path.Join("templates", "layout.html")
-	fp := path.Join("templates", r.URL.Path)
+	fp := path.Join("templates", "index.html")
+
 	// Return a 404 if the template doesn't exist
 	info, err := os.Stat(fp)
 	if err != nil {
@@ -43,7 +103,7 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "layout", todos); err != nil {
 		log.Println(err.Error())
 		http.Error(w, http.StatusText(500), 500)
 	}
